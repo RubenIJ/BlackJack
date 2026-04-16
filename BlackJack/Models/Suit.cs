fe428e2e9f435458ede04d141e6546d15dcc4b1d
SadDisplay <1212236@student.roc-nijmegen.nl>
2026-04-16 13:40:25 +0200

    Fix BlackJack submodule naar gewone map

---
+++ BlackJack/Models/Suit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackJack.Models
+{
+    public enum Suit
+    {
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+}
