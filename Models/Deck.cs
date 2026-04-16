85f821e1df1e11996abfdeb6aaab80d4f535637c
SadDisplay <1212236@student.roc-nijmegen.nl>
2026-04-16 13:48:59 +0200

    Skelet aangemaakt

---
+++ Models/Deck.cs
@@ -0,0 +1,45 @@
+﻿namespace BlackJack.Models
+{
+    public class Deck
+    {
+        private List<Card> cards = new List<Card>();
+        public int CardsLeft => cards.Count;
+
+        public Deck()
+        {
+            int[] values = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11 };
+
+            foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+            {
+                int i = 0;
+                foreach (Rank rank in Enum.GetValues(typeof(Rank)))
+                {
+                    cards.Add(new Card(suit, rank, values[i]));
+                    i++;
+                }
+            }
+        }
+
+        public void Shuffle()
+        {
+            Random rng = new Random();
+            int n = cards.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                Card temp = cards[k];
+                cards[k] = cards[n];
+                cards[n] = temp;
+            }
+        }
+
+        public Card? Draw()
+        {
+            if (cards.Count == 0) return null;
+            Card drawn = cards[0];
+            cards.RemoveAt(0);
+            return drawn;
+        }
+    }
+}
---
+++ Models/Hand.cs
@@ -0,0 +1,43 @@
+﻿namespace BlackJack.Models
+{
+    public class Hand
+    {
+        private List<Card> cards = new List<Card>();
+        private bool hasPassed = false;
+
+        public bool CanPlay { get; private set; } = true;
+        public int Total { get; private set; } = 0;
+
+        public bool Hit(Card card)
+        {
+            cards.Add(card);
+            Total += card.Value;
+
+            // Aas van 11 naar 1 als je over 21 gaat
+            if (Total > 21)
+            {
+                foreach (Card c in cards)
+                {
+                    if (c.Value == 11)
+                    {
+                        Total -= 10;
+                        break;
+                    }
+                }
+            }
+
+            if (Total > 21)
+            {
+                CanPlay = false;
+            }
+
+            return CanPlay;
+        }
+
+        public void Stand()
+        {
+            hasPassed = true;
+            CanPlay = false;
+        }
+    }
+}
